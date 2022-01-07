0cbcfdfffcf648032961c060cb44256023e3bcf8
Khojiakbar <khayotovkhojiakbar@gmail.com>
2022-01-07 21:47:40 +0500

    Initial Commit

--- main.py
+++ main.py
@@ -208,13 +208,13 @@ def edit_post(post_id):
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        body=post.body
+        body=post.body,
+        author=current_user,
     )
     if edit_form.validate_on_submit():
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = current_user.name
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
