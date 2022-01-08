d5e42a7476502cb5fdc007f2852ce810242d12fe
Khojiakbar <khayotovkhojiakbar@gmail.com>
2022-01-08 16:19:17 +0500

    change from sqlite to postgres

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ print(os.environ.get("SECRET_KEY"))
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
+app.config['SECRET_KEY'] = "super secret key"
+
 # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
